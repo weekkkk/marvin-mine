b58a523004eb02a3955b92d9deb0e92a744ed228
AlexeyFrunza <tvoih8r@mail.ru>
2025-01-21 01:19:25 +0200

    [sCxIkxVY] Создать компоненты - Кнопка -- добавил в папку shared/ui компонент button -- описал интерфейс для пропсов -- добавил стили для активного/неактивного состояния

---
+++ src/shared/ui/button/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import type { UiButtonProps } from "./types";
+
+const props = withDefaults(defineProps<UiButtonProps>(), {active: false});
+
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+
+const slots = useSlots();
+
+const config = useRuntimeConfig();
+</script>
+<template>
+  <button
+    @click="emit('click')"
+    :class="[
+      'w-[6.53rem] h-[3.33rem] rounded-2xl border-spruce border-[0.05rem] drop-shadow-outer-bottom',
+      {'bg-olive text-spruce shadow-inner-top border-spruce': active},
+      {'bg-black text-beige shadow-inner-top-light border-spruce': !active}
+    ]"
+  >
+    <slot />
+  </button>
+</template>
