fba13565befe9c1fbf3907fc8179694b7334c499
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-01-20 21:56:29 +0200

    [hRmzsNd5] [Z6YRDWcN] Инициализовать проект

---
+++ nuxt.config.ts
@@ -0,0 +1,95 @@
+const formatComponentName = (
+  pascalName: string,
+  postfix: string = "",
+  skip: number = 0
+) =>
+  `${pascalName.slice(0, skip)}${pascalName
+    .slice(skip)
+    .replaceAll("Ui", "")}${postfix}`;
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ["./src/app/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  components: {
+    dirs: [
+      {
+        path: "~/src/pages",
+        extendComponent(component) {
+          component.pascalName = formatComponentName(component.pascalName, "");
+          return component;
+        },
+        pattern: "**/*index.vue",
+        extensions: ["vue"],
+      },
+      {
+        path: "~/src/widgets",
+        extendComponent(component) {
+          component.pascalName = formatComponentName(
+            component.pascalName,
+            "Widget"
+          );
+          return component;
+        },
+        pattern: "**/*index.vue",
+        extensions: ["vue"],
+      },
+      {
+        path: "~/src/features",
+        extendComponent(component) {
+          component.pascalName = formatComponentName(
+            component.pascalName,
+            "Feature"
+          );
+          return component;
+        },
+        pattern: "**/*index.vue",
+        extensions: ["vue"],
+      },
+      {
+        path: "~/src/entities",
+        extendComponent(component) {
+          component.pascalName = formatComponentName(component.pascalName);
+          return component;
+        },
+        pattern: "**/*index.vue",
+        extensions: ["vue"],
+      },
+      {
+        path: "~/src/shared",
+
+        extendComponent(component) {
+          component.pascalName = formatComponentName(
+            component.pascalName,
+            "",
+            2
+          );
+          return component;
+        },
+        pattern: "**/*index.vue",
+        extensions: ["vue"],
+      },
+    ],
+  },
+  dir: {
+    pages: "./src/app/routes",
+    layouts: "./src/app/layouts",
+    assets: "./src/app/assets",
+    middleware: "./src/app/middlewares",
+    app: "./src/app",
+  },
+  imports: {
+    dirs: [
+      "./src/widgets/*/*/index.ts",
+      "./src/entities/*/*/index.ts",
+      "./src/shared/*/index.ts",
+    ],
+  },
+  modules: ["nuxt-keen-slider"],
+});
